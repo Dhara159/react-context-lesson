5529639a3f66095dff6cb17cce772cdf2e6ab0f1
Dhara Vihol <viholdhara@gmail.com>
2020-02-21 15:44:34 +0530

    Used Context API for collection component using useContext hook

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,37 +1,23 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useContext } from 'react';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 import CollectionContext from './../../contexts/collection.context';
 
-import { selectCollection } from '../../redux/shop/shop.selectors';
-
 import './collection.styles.scss';
 
 const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionContext)
+  const { title, items } = collections[match.params.collectionId];
   return (
-    <CollectionContext.Consumer>
-      {
-        collections => {
-          const { title, items } = collections[match.params.collectionId];
-          return (
-            <div className='collection-page'>
-              <h2 className='title'>{title}</h2>
-              <div className='items'>
-                {items.map(item => (
-                  <CollectionItem key={item.id} item={item} />
-                ))}
-              </div>
-            </div>
-          );
-        }
-      }
-    </CollectionContext.Consumer>
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
